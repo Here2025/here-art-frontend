99d289d6e53a4a3e9bdfed1287dd4534ec3c7835
Here2025 <zmulwanda@gmail.com>
2026-05-20 23:27:01 -0400

    Load HERE app styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './app-theme.css';
+import './app-layout.css';
+import './app-components.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
