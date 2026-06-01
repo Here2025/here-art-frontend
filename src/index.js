b02b930ab418686e42a8c700d07646d18c64e52e
Here2025 <zmulwanda@gmail.com>
2026-06-01 13:11:53 -0400

    Use final HERE redesign stylesheet

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './app-theme.css';
-import './app-layout.css';
-import './app-components.css';
-import './app-map.css';
-import './app-visuals.css';
-import './app-platform.css';
-import './app-profiles.css';
+import './app-redesign.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
