fd475f4beee0ddd3059e54a336978a00bb893e3a
Here2025 <zmulwanda@gmail.com>
2026-06-01 14:12:22 -0400

    Import HERE interaction layout fixes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import './app-redesign.css';
 import './app-web.css';
+import './app-interactions.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
