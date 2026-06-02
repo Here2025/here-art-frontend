f2194aae60bfeba622ba791c00ec6bb320d5a7c4
Here2025 <zmulwanda@gmail.com>
2026-06-01 20:45:04 -0400

    Import HERE navigation scroll helper

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import './app-map-look.css';
 import './app-create.css';
 import './app-footer.css';
 import './app-typography.css';
+import './app-navigation-scroll.js';
 import './app-footer.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
